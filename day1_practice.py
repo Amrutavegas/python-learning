d0061753a737ed81c886a5783755f7aa8bb63a1a
Rue <amrutabalande11@gmail.com>
2026-09-09 11:13:42 +0530

    Day 1 : Python basics practice

---
+++ day1_practice.py
@@ -0,0 +1,35 @@
+1. name=input("Enter your name : ")
+   age=input("Enter your age : ")
+   print(f"Hello {name},you are {age} years old ")
+
+2.  n1=int(input("Enter num 1:"))
+    n2=int(input("Enter num 2:"))
+    print(f"Sum={n1+n2}")
+    print(f"Differene={n1-n2}")
+    print(f"Product={n1*n2}")
+    print(f"Division={n1/n2}")
+
+3.  c=int(input("Enter Tempreture in Celsius:"))
+    f=(c*9/5)+32
+    print(f"The Tempreture of given celsius tempreture in farhenheit :{f}")
+
+4.   m1=float(input("Enter marks of subject 1:"))
+     m2=float(input("Enter marks of subject 2:"))
+     m3=float(input("Enter marks of subject 3:"))
+     Total=m1+m2+m3
+     print(f"Total of all subjects :{Total}")
+     print(f"Your Percentage :{(Total/300)*100}")
+
+5.   a=5
+     print(f"Square:{a*a}")
+     print(f"cube:{a*a*a}")
+
+6.    name=input("Enter your Name :")
+      age=int(input("Enter your age:"))
+      College=input("Enter your College:")
+      percent=float(input("Enter your Percentage:"))
+      print("-----Student Profile-----")
+      print(f"Name : {name}")
+      print(f"Age : {age}")
+      print(f"College : {College}")
+      print(f"Percentage : {percent}")
